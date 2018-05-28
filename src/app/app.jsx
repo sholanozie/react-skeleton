90881362d738de58938f878f8999fa9d78211d72
Shola Anozie <shola@soulprovidr.co>
2018-05-27 22:18:15 -0500

    Updated to use webpack.

---
+++ src/app/app.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <img
+        className="skull"
+        src="/img/skull.svg"
+        alt="https://shopcraftables.com/products/Free-Skull--SVG-Cut-File/"
+      />
+    );
+  }
+}
+
+export default App;
--- src/components/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const App = React.createClass({
-  render() {
-    return <h1>react-skeleton</h1>;
-  }
-});
-
-export default App;
